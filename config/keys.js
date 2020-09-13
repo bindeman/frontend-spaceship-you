fb47b8a0373d34f2c4d0efe9c916a795735fa4fc
Phillip Bindeman <9490743+bindeman@users.noreply.github.com>
2020-09-13 01:51:29 -0700

    add files back

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://phil:notsecure123@database-tuqyt.mongodb.net/spaceship-you?retryWrites=true&w=majority'
+
+}
---
+++ models/GraphData.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+//const LayerSchema = require('Layer');
+
+// const LayerSchema = new Schema({
+
+//     name: {
+//         type: String,
+//         //required: false
+//     },
+//     id: {
+//         type: Number,
+//         //required: true
+//     },
+//     type: {
+//         type: String,
+//         //required: true
+//     },
+//     activation: {
+//         type: String,
+//         //required: true
+//     },
+//     width: {
+//         type: Number,
+//         //required: true
+//     }
+
+// });
+
+// Create Schema
+const NetworkSchema = new Schema({
+
+    name: {
+        type: String,
+        required: false,
+        default: "Untitled"
+    },
+    data: [{
+    
+        type: Number,
+        //required: false
+    }]
+});
+
+module.exports = Network = mongoose.model('network', NetworkSchema);
